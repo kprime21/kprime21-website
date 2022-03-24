f9853dfa6b92e083fc742e0fafc7732301921253
kprime21 <kohuladaskirjan@gmail.com>
2022-03-24 12:59:05 -0400

    first commit

---
+++ styles/Home.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+.container {
+  padding: 0 2rem;
+  min-height: 100vh;
+  padding: 4rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+`;
